7f5ecdc74c6c2bba9c82d1a921c93f842bb3f659
Ken-Sumi1019 <ken.sumi1019@gmail.com>
2024-08-19 13:44:46 +0900

    ちょっと追加

--- tree/tree.go
+++ tree/tree.go
@@ -1,4 +1,16 @@
 package tree
 
 type Node struct {
+	tag        string
+	attributes map[string]string
+	text       string
+	children   []*Node
+}
+
+func (parent *Node) AddChild(child *Node) {
+	parent.children = append(parent.children, child)
+}
+
+func (parent *Node) Search(equalFn func(*Node) bool) *Node {
+	return nil
 }
