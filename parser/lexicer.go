21b5ee2b0b9e99409f6e86f8b0362ec2276ac560
Ken-Sumi1019 <ken.sumi1019@gmail.com>
2024-08-19 16:15:04 +0900

    属性の存在するxmlを字句解析できるようにする

--- parser/lexicer.go
+++ parser/lexicer.go
@@ -13,27 +13,32 @@ var (
 	SLASH uint8 = 4 // /
 )
 
-func Analicys(xml string) []*Token {
+func Analicys(xml string) []Token {
 	lex := lexicer{text: xml, index: 0}
-	tokens := []*Token{}
+	tokens := []Token{}
 
 	for lex.index < len(xml) {
 		switch lex.nextChar() {
 		case '<':
-			tokens = append(tokens, &Token{Kind: LAB})
+			tokens = append(tokens, Token{Kind: LAB})
 			lex.next()
 		case '>':
-			tokens = append(tokens, &Token{Kind: RAB})
+			tokens = append(tokens, Token{Kind: RAB})
 			lex.next()
 		case '=':
-			tokens = append(tokens, &Token{Kind: EQ})
+			tokens = append(tokens, Token{Kind: EQ})
 			lex.next()
 		case '/':
-			tokens = append(tokens, &Token{Kind: SLASH})
+			tokens = append(tokens, Token{Kind: SLASH})
 			lex.next()
+		case ' ', '\n', '\r':
+			lex.next()
+		case '\'', '"':
+			text := lex.takeOutTextInQuotes()
+			tokens = append(tokens, Token{Kind: TEXT, Value: text})
 		default:
 			text := lex.takeOutText()
-			tokens = append(tokens, &Token{Kind: TEXT, Value: text})
+			tokens = append(tokens, Token{Kind: TEXT, Value: text})
 		}
 	}
 
@@ -56,8 +61,19 @@ func (lex *lexicer) next() int {
 
 func (lex *lexicer) takeOutText() string {
 	firstIdx := lex.index
-	for lex.nextChar() != '<' && lex.nextChar() != '>' {
+	for lex.nextChar() != '<' && lex.nextChar() != '>' && lex.nextChar() != ' ' && lex.nextChar() != '=' {
 		lex.next()
 	}
+	return lex.text[firstIdx:lex.index]
+}
+
+func (lex *lexicer) takeOutTextInQuotes() string {
+	firstIdx := lex.index
+	quote := lex.nextChar()
+	lex.next()
+	for lex.nextChar() != quote {
+		lex.next()
+	}
+	lex.next()
 	return lex.text[firstIdx:lex.index]
 }
--- parser/lexicert_test.go
+++ parser/lexicert_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestTypical(t *testing.T) {
 	xml := "<html>konichiha</html>"
-	target := []*parser.Token{
+	target := []parser.Token{
 		{Kind: parser.LAB},
 		{Kind: parser.TEXT, Value: "html"},
 		{Kind: parser.RAB},
@@ -28,15 +28,15 @@ func TestTypical(t *testing.T) {
 
 func TestAttribute(t *testing.T) {
 	xml := "<html key=\"value\" key2=\"value2\">konichiha</html>"
-	target := []*parser.Token{
+	target := []parser.Token{
 		{Kind: parser.LAB},
 		{Kind: parser.TEXT, Value: "html"},
 		{Kind: parser.TEXT, Value: "key"},
 		{Kind: parser.EQ},
-		{Kind: parser.TEXT, Value: "value"},
+		{Kind: parser.TEXT, Value: "\"value\""},
 		{Kind: parser.TEXT, Value: "key2"},
 		{Kind: parser.EQ},
-		{Kind: parser.TEXT, Value: "value2"},
+		{Kind: parser.TEXT, Value: "\"value2\""},
 		{Kind: parser.RAB},
 		{Kind: parser.TEXT, Value: "konichiha"},
 		{Kind: parser.LAB},
