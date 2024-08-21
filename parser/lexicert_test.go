8670f9bf135ed6b60520f72b914c96aac40df958
Ken-Sumi1019 <ken.sumi1019@gmail.com>
2024-08-21 09:20:06 +0900

    テストコードの修正

--- parser/lexicert_test.go
+++ parser/lexicert_test.go
@@ -1,6 +1,7 @@
 package parser_test
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"xml-parser/parser"
@@ -8,46 +9,54 @@ import (
 
 func TestTypical(t *testing.T) {
 	xml := "<html>konichiha</html>"
-	target := []parser.Token{
-		{Kind: parser.LAB},
-		{Kind: parser.TEXT, Value: "html"},
-		{Kind: parser.RAB},
-		{Kind: parser.TEXT, Value: "konichiha"},
-		{Kind: parser.LAB},
-		{Kind: parser.SLASH},
-		{Kind: parser.TEXT, Value: "html"},
-		{Kind: parser.RAB},
+	target := []*parser.Token{
+		&parser.Token{Kind: parser.LAB},
+		&parser.Token{Kind: parser.TEXT, Value: "html"},
+		&parser.Token{Kind: parser.RAB},
+		&parser.Token{Kind: parser.TEXT, Value: "konichiha"},
+		&parser.Token{Kind: parser.LAB},
+		&parser.Token{Kind: parser.SLASH},
+		&parser.Token{Kind: parser.TEXT, Value: "html"},
+		&parser.Token{Kind: parser.RAB},
 	}
 	tokens := parser.Analicys(xml)
 	t.Run("typical", func(t *testing.T) {
 		if !reflect.DeepEqual(tokens, target) {
-			t.Errorf("%+v : %+v", xml, tokens)
+			t.Errorf("%+v : %+v", xml, tokensToString(tokens))
 		}
 	})
 }
 
 func TestAttribute(t *testing.T) {
 	xml := "<html key=\"value\" key2=\"va\\\"lue2\">konichiha</html>"
-	target := []parser.Token{
-		{Kind: parser.LAB},
-		{Kind: parser.TEXT, Value: "html"},
-		{Kind: parser.TEXT, Value: "key"},
-		{Kind: parser.EQ},
-		{Kind: parser.TEXT, Value: "\"value\""},
-		{Kind: parser.TEXT, Value: "key2"},
-		{Kind: parser.EQ},
-		{Kind: parser.TEXT, Value: "\"va\\\"lue2\""},
-		{Kind: parser.RAB},
-		{Kind: parser.TEXT, Value: "konichiha"},
-		{Kind: parser.LAB},
-		{Kind: parser.SLASH},
-		{Kind: parser.TEXT, Value: "html"},
-		{Kind: parser.RAB},
+	target := []*parser.Token{
+		&parser.Token{Kind: parser.LAB},
+		&parser.Token{Kind: parser.TEXT, Value: "html"},
+		&parser.Token{Kind: parser.TEXT, Value: "key"},
+		&parser.Token{Kind: parser.EQ},
+		&parser.Token{Kind: parser.TEXT, Value: "\"value\""},
+		&parser.Token{Kind: parser.TEXT, Value: "key2"},
+		&parser.Token{Kind: parser.EQ},
+		&parser.Token{Kind: parser.TEXT, Value: "\"va\\\"lue2\""},
+		&parser.Token{Kind: parser.RAB},
+		&parser.Token{Kind: parser.TEXT, Value: "konichiha"},
+		&parser.Token{Kind: parser.LAB},
+		&parser.Token{Kind: parser.SLASH},
+		&parser.Token{Kind: parser.TEXT, Value: "html"},
+		&parser.Token{Kind: parser.RAB},
 	}
 	tokens := parser.Analicys(xml)
 	t.Run("typical", func(t *testing.T) {
 		if !reflect.DeepEqual(tokens, target) {
-			t.Errorf("%+v : %+v", xml, tokens)
+			t.Errorf("%+v : %+v", xml, tokensToString(tokens))
 		}
 	})
+}
+
+func tokensToString(tokens []*parser.Token) string {
+	result := ""
+    for _,v :=range tokens {
+        result += fmt.Sprintf("%+v", *v)
+    }
+    return result
 }
