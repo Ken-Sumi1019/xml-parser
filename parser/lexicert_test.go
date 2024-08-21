4dfc043e8dfe6f111880078ff3c9a6ef8e409697
Ken-Sumi1019 <ken.sumi1019@gmail.com>
2024-08-21 10:39:15 +0900

    冗長な記述を削除

--- parser/lexicert_test.go
+++ parser/lexicert_test.go
@@ -10,14 +10,14 @@ import (
 func TestTypical(t *testing.T) {
 	xml := "<html>konichiha</html>"
 	target := []*parser.Token{
-		&parser.Token{Kind: parser.LAB},
-		&parser.Token{Kind: parser.TEXT, Value: "html"},
-		&parser.Token{Kind: parser.RAB},
-		&parser.Token{Kind: parser.TEXT, Value: "konichiha"},
-		&parser.Token{Kind: parser.LAB},
-		&parser.Token{Kind: parser.SLASH},
-		&parser.Token{Kind: parser.TEXT, Value: "html"},
-		&parser.Token{Kind: parser.RAB},
+		{Kind: parser.LAB},
+		{Kind: parser.TEXT, Value: "html"},
+		{Kind: parser.RAB},
+		{Kind: parser.TEXT, Value: "konichiha"},
+		{Kind: parser.LAB},
+		{Kind: parser.SLASH},
+		{Kind: parser.TEXT, Value: "html"},
+		{Kind: parser.RAB},
 	}
 	tokens := parser.Analicys(xml)
 	t.Run("typical", func(t *testing.T) {
@@ -30,20 +30,20 @@ func TestTypical(t *testing.T) {
 func TestAttribute(t *testing.T) {
 	xml := "<html key=\"value\" key2=\"va\\\"lue2\">konichiha</html>"
 	target := []*parser.Token{
-		&parser.Token{Kind: parser.LAB},
-		&parser.Token{Kind: parser.TEXT, Value: "html"},
-		&parser.Token{Kind: parser.TEXT, Value: "key"},
-		&parser.Token{Kind: parser.EQ},
-		&parser.Token{Kind: parser.TEXT, Value: "\"value\""},
-		&parser.Token{Kind: parser.TEXT, Value: "key2"},
-		&parser.Token{Kind: parser.EQ},
-		&parser.Token{Kind: parser.TEXT, Value: "\"va\\\"lue2\""},
-		&parser.Token{Kind: parser.RAB},
-		&parser.Token{Kind: parser.TEXT, Value: "konichiha"},
-		&parser.Token{Kind: parser.LAB},
-		&parser.Token{Kind: parser.SLASH},
-		&parser.Token{Kind: parser.TEXT, Value: "html"},
-		&parser.Token{Kind: parser.RAB},
+		{Kind: parser.LAB},
+		{Kind: parser.TEXT, Value: "html"},
+		{Kind: parser.TEXT, Value: "key"},
+		{Kind: parser.EQ},
+		{Kind: parser.TEXT, Value: "\"value\""},
+		{Kind: parser.TEXT, Value: "key2"},
+		{Kind: parser.EQ},
+		{Kind: parser.TEXT, Value: "\"va\\\"lue2\""},
+		{Kind: parser.RAB},
+		{Kind: parser.TEXT, Value: "konichiha"},
+		{Kind: parser.LAB},
+		{Kind: parser.SLASH},
+		{Kind: parser.TEXT, Value: "html"},
+		{Kind: parser.RAB},
 	}
 	tokens := parser.Analicys(xml)
 	t.Run("typical", func(t *testing.T) {
