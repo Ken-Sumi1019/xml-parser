5dc6017f2ceba855b1b7224083a7e2574c5001cc
Ken-Sumi1019 <ken.sumi1019@gmail.com>
2024-08-23 22:39:36 +0900

    ネストして属性もついているテストを追加

--- parser/lexicert_test.go
+++ parser/lexicert_test.go
@@ -110,6 +110,45 @@ func TestNest(t *testing.T) {
 	})
 }
 
+func TestNestWithAttribute(t *testing.T) {
+	xml := `
+    <html key="value" key2="va\"lue2">
+        <body>
+            konichiha
+        </body>
+    </html>
+    `
+	target := []*parser.Token{
+		{Kind: parser.LAB},
+		{Kind: parser.TEXT, Value: "html"},
+		{Kind: parser.TEXT, Value: "key"},
+		{Kind: parser.EQ},
+		{Kind: parser.TEXT, Value: "value"},
+		{Kind: parser.TEXT, Value: "key2"},
+		{Kind: parser.EQ},
+		{Kind: parser.TEXT, Value: "va\"lue2"},
+		{Kind: parser.RAB},
+		{Kind: parser.LAB},
+		{Kind: parser.TEXT, Value: "body"},
+		{Kind: parser.RAB},
+		{Kind: parser.TEXT, Value: "konichiha"},
+		{Kind: parser.LAB},
+		{Kind: parser.SLASH},
+		{Kind: parser.TEXT, Value: "body"},
+		{Kind: parser.RAB},
+		{Kind: parser.LAB},
+		{Kind: parser.SLASH},
+		{Kind: parser.TEXT, Value: "html"},
+		{Kind: parser.RAB},
+	}
+	tokens := parser.Lexical(xml)
+	t.Run("typical", func(t *testing.T) {
+		if !reflect.DeepEqual(tokens, target) {
+			t.Errorf("%+v : %+v", xml, tokensToString(tokens))
+		}
+	})
+}
+
 func tokensToString(tokens []*parser.Token) string {
 	result := ""
     for _,v :=range tokens {
